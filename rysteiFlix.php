299060923f12e26f938eb9db9fdb4131513d40aa
rystei <gustavo.telles@edu.unifil.br>
2025-05-29 10:05:12 -0300

    Projeto stream rysteiFlix

---
+++ rysteiFlix.php
@@ -0,0 +1,12 @@
+<?php
+
+echo "Bem-vindo(a) ao rysteiFlix sua plataforma de Streamer \n" ;
+
+$nomeFilme = "Top Gun - Maverick";
+$anoLancamento = 2022;
+$notaFilme = 8.1;
+$incluidoNoPlano = true;
+
+echo $nomeFilme;
+
+
