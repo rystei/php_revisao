d8fa98900dad386b522df5f884082222b0ca5f13
Gustavo Cardoso Telles <gustavo.telles@edu.unifil.br>
2025-06-10 10:34:30 -0300

    match

--- criandoSuaAplicação/rysteiFlix.php
+++ criandoSuaAplicação/rysteiFlix.php
@@ -3,6 +3,9 @@
 echo "Bem-vindo(a) ao rysteiFlix sua plataforma de Streamer \n" ;
 
 $nomeFilme = "Top Gun - Maverick";
+$nomeFilme = "Gente Grande";
+$nomeFilme = "Thor - Ragnarok";
+
 $anoLancamento = $argv[1] ?? 2022;
 
 $somaNotas = 9 ;
@@ -20,4 +23,20 @@ echo "Nome do filme: " . $nomeFilme ,"\n";
 echo "Nota do filme: " . $notaFilme , "\n";
 echo "Ano de lançamento: " . $anoLancamento, "\n";
 
+if ($anoLancamento > 2022) {
+    echo"esse filme é um lançamento \n";
+} elseif ($anoLancamento > 2020 && $anoLancamento <= 2022) {
+    echo"esse filme ainda é novo \n";
+} else {
+    echo"esse filme não é um lançamento \n";
+}
 
+
+$genero = match ($nomeFilme) {
+    "Top Gun - Maverick" => "ação",
+    "Thor - Ragnarok" => "Super-Heroi",
+    "Gente Grande" => "Comedia",
+    default => "Gênero Desconhecido",
+};
+
+echo "O gênero do filme é: $genero \n";
