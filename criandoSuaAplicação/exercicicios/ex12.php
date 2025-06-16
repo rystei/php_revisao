6fb0d606bea17f56d24fcc86230b68a8793078bf
Gustavo Cardoso Telles <gustavo.telles@edu.unifil.br>
2025-06-16 11:50:11 -0300

    add array

---
+++ criandoSuaAplicação/exercicicios/ex12.php
@@ -0,0 +1,6 @@
+<?php
+//Crie um array contendo nomes de familiares seus e após sua criação adicione mais elementos ao array.
+
+$familiares = ['bruno', 'tania', 'vitor', 'maria'];
+$familiares [] = "alguem";
+echo var_dump($familiares);
